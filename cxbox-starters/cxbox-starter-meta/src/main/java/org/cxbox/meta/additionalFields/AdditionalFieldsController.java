3001ac7410be8082a548fadce0715e90875264f7
cxbox <cxbox.mail@gmail.com>
2024-03-01 06:57:50 +0300

    personalAdditionalFields delete operation api fix

--- cxbox-starters/cxbox-starter-meta/src/main/java/org/cxbox/meta/additionalFields/AdditionalFieldsController.java
+++ cxbox-starters/cxbox-starter-meta/src/main/java/org/cxbox/meta/additionalFields/AdditionalFieldsController.java
@@ -27,7 +27,6 @@ import org.cxbox.core.util.ResponseBuilder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -49,7 +48,7 @@ public class AdditionalFieldsController {
 	}
 
 	@RequestMapping(method = RequestMethod.DELETE)
-	public void deleteAdditionalFields(@RequestParam RequestDTO<List<Long>> ids) {
+	public void deleteAdditionalFields(@RequestBody RequestDTO<List<Long>> ids) {
 		additionalFieldsService.deleteAdditionalFields(ids.getData());
 		cxboxCachingService.evictUserCache(); //TODO>>REMOVE ONLY CURRENT USER CACHE!!!!
 	}
