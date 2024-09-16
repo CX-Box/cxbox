0c04001ad533ffc8fbd66d5dfb10bd4fd3982cef
Andrew Zaharenko <113582220+Kotman242@users.noreply.github.com>
2024-09-16 11:42:50 +0300

    CXBOX-421 | Added WidgetTypeFamily for the WidgetName annotation (#48)
    * CXBOX-421 | The WidgetName annotation has been added
    
    * CXBOX-421 | Added WidgetTypeFamily for the WidgetName annotation

---
+++ cxbox-starters/cxbox-starter-meta/src/main/java/org/cxbox/meta/ui/field/WidgetTypeFamily.java
@@ -0,0 +1,23 @@
+package org.cxbox.meta.ui.field;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum WidgetTypeFamily {
+
+	INFO("Info"),
+	FORM("Form"),
+	LIST("List"),
+	GROUPING_HIERARCHY("GroupingHierarchy"),
+	STATS_BLOCK("StatsBlock"),
+	ASSOC_LIST_POPUP("AssocListPopup"),
+	PICK_LIST_POPUP("PickListPopup"),
+	FORM_POPUP("FormPopup"),
+	HEADER_WIDGET("HeaderWidget"),
+	LEVEL_MENU("LevelMenu"),
+	STEPS("Steps");
+
+	private final String value;
+}
