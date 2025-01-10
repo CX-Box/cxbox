0c438fe529e61ca5294e34a9938e1bc078f5e5ce
Andrew Zaharenko <113582220+Kotman242@users.noreply.github.com>
2025-01-10 16:46:17 +0300

    CXBOX-646 | Sidebar menu word wrap. Part 1 of 2 (Backend) (#63)
    * CXBOX-644 | added the property of transferring values to the sidebar
    
    * CXBOX-646 | refactoring

--- cxbox-core/src/main/java/org/cxbox/core/config/properties/UIProperties.java
+++ cxbox-core/src/main/java/org/cxbox/core/config/properties/UIProperties.java
@@ -25,21 +25,34 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("cxbox.ui")
 public class UIProperties {
 
+	public static final String SIDE_BAR_WORD_BREAK = "sideBarWordBreak";
+
+	/**
+	 * * useServletContextPath = true is deprecated, and it means you will create 2 servlets (for api with context-path = '/api/v1' and for ui with context-path = '/ui'). This is very complex and non-common approach for springboot apps.
+	 * useServletContextPath = false, means your app have only 1 servlet with context-path = '' , so cxbox needs to add '/api/v1' prefix to rest controllers explicitly. Also cxbox will configure ui static content delivery in a slightly different way
+	 */
+	@Deprecated
+	private Boolean useServletContextPath = false;
 	public static final String DRILL_DOWN_TOOLTIP_NAME = "drillDownTooltip";
 
 	public static final String MULTI_ROLE_ENABLED  = "multiRoleEnabled";
 
 	/**
-	 ** Use only when useServletContextPath = false;
+	 * * Use only when useServletContextPath = false;
 	 */
 	private String path = "/ui";
 
 	/**
-	 ** Lock row in db timeout in ms; -1 by default, e.g. infinite
+	 * * Lock row in db timeout in ms; -1 by default, e.g. infinite
 	 */
 	private int uiLockTimeoutMs = -1;
 
 	private String systemUrl = null;
+
+	/**
+	 * Enabled the transfer of long names of side menu values
+	 */
+	private String sideBarWordBreak = "none";
 
 	private String drillDownTooltip = null;
 
