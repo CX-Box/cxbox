6a2ca10073880ae4dd455fe3931d2e6a7bbc633c
Andrew Zaharenko <113582220+Kotman242@users.noreply.github.com>
2025-06-25 11:24:59 +0300

    CXBOX-935 | ClassifyDataProvider JavaDoc (#91)
    * CXBOX-935 | ClassifyDataProvider JavaDoc

--- cxbox-core/src/main/java/org/cxbox/core/util/filter/provider/ClassifyDataProvider.java
+++ cxbox-core/src/main/java/org/cxbox/core/util/filter/provider/ClassifyDataProvider.java
@@ -43,11 +43,56 @@ public interface ClassifyDataProvider {
 	List<ClassifyDataParameter> getClassifyDataParameters(Field dtoField, FilterParameter filterParam,
 			SearchParameter searchParam, List<ClassifyDataProvider> providers);
 
+	/**
+	 * Retrieves a filter predicate based on the provided search operation and criteria.
+	 *
+	 * <p>Override this method in implementing classes to provide custom filtering logic.
+	 * Default implementation returns null (no filtering).</p>
+	 *
+	 * <p><b>Example for LocalTime filtering:</b></p>
+	 * <pre>{@code
+	 * if (value instanceof LocalTime) {
+	 *     Expression<?> timeExpr = getExpressionByTimePart(field, cb, dialect);
+	 *     Comparable<?> comparableValue = MetadataUtils.requireComparable(value);
+	 *
+	 *     switch (operator) {
+	 *         case EQUALS: return cb.equal(timeExpr, comparableValue);
+	 *         case GREATER_OR_EQUAL_THAN: return cb.greaterThanOrEqualTo(timeExpr, comparableValue);
+	 *         case LESS_OR_EQUAL_THAN: return cb.lessThanOrEqualTo(timeExpr, comparableValue);
+	 *         default: return null;
+	 *     }
+	 * }
+	 * return null;
+	 * }</pre>
+	 * <p>This method is intended to be overridden in the implementing class (Provider)
+	 * if custom sorting logic is required. If this method is not implemented,
+	 * the default sorting mechanism will be used.</p>
+	 */
 	default Predicate getFilterPredicate(@NonNull SearchOperation operator, @NonNull Root<?> root, @NonNull CriteriaBuilder cb,
 			@NonNull ClassifyDataParameter criteria, @NonNull Path field, @NonNull Object value, @NonNull DialectName dialect) {
 		return null;
 	}
 
+	/**
+	 * Retrieves a sort expression based on the provided search parameter and criteria.
+	 *
+	 * <p>Override this method to provide custom sorting logic. Default returns null (default sorting).</p>
+	 *
+	 * <p><b>Example for time value sorting:</b></p>
+	 * <pre>{@code
+	 * if (searchParameter.provider() == TimeValueProvider.class) {
+	 *     return dialect == DialectName.ORACLE
+	 *         ? builder.function("TO_CHAR", String.class, fieldPath, builder.literal("HH24:MI:SS"))
+	 *         : fieldPath.as(LocalTime.class);
+	 * }
+	 * return null;
+	 * }</pre>
+	 *
+	 * <p>This method is intended to be overridden in the implementing class (Provider)
+	 * if custom sorting logic is required. If this method is not implemented,
+	 * the default sorting mechanism will be used.</p>
+	 *
+	 */
 	default Expression<?> getSortExpression(@NonNull final SearchParameter searchParameter, @NonNull final CriteriaBuilder builder,
 			@NonNull final CriteriaQuery query, @NonNull final Root<?> root, @NonNull final Class dtoClazz, @NonNull Path fieldPath, @NonNull DialectName dialect) {
 		return null;
