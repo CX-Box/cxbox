db927d97a99324b9b60feafd95fb48ef605d7bef
agelfman <agelfman@cxlab.ru>
2025-07-01 13:41:29 +0300

    CXBOX-812 Add changedNow for fields Part1

--- cxbox-core/src/main/java/org/cxbox/core/service/ResponseFactory.java
+++ cxbox-core/src/main/java/org/cxbox/core/service/ResponseFactory.java
@@ -88,10 +88,10 @@ public class ResponseFactory {
 
 		DataResponseDTO dataResponseDTO;
 		if (changedNowMap == null) {
-			dataResponseDTO = getDTOFromMapInner(map, clazz, bc, ignoreBusinessErrors);
+			dataResponseDTO = getDTOFromMapInner(dataMap == null ? map : dataMap, clazz, bc, ignoreBusinessErrors);
 		} else {
 			dataResponseDTO = getDTOFromMapInner(dataMap, clazz, bc, ignoreBusinessErrors);
-			DataResponseDTO changedNowDTO = getDTOFromMapInner(dataMap,clazz,bc,ignoreBusinessErrors);
+			DataResponseDTO changedNowDTO = getDTOFromMapInner(changedNowMap,clazz,bc,ignoreBusinessErrors);
 			CnangedNowParam cnangedNowParam = new CnangedNowParam();
 			cnangedNowParam.setChangedNowDTO(changedNowDTO);
 			cnangedNowParam.setChangedNow(changedNowMap.keySet());
