0c2732b33a6ecb658b9baee9ad18f25380e9fbfa
IgorNB <igornb.mail@gmail.com>
2025-07-07 00:33:43 +0300

    CXBOX-849 | core support for offically introduced in previous releases widget and field types (moved from demo project) (#95)
    * CXBOX-849 |
    0. moved "multipleSelect" support from the demo project to core.
    1. moved "suggestionPickList" support from the demo project to core.
    2. moved prev releases officially introduced widget types from demo to core.
    3. merged InfoFieldExtractor.java and SimpleFormFieldExtractor.java to new single default SimpleFieldExtractor.java
    
    ---------
    
    Co-authored-by: cxbox <cxbox.mail@gmail.com>

--- cxbox-starters/cxbox-starter-meta/src/main/java/org/cxbox/meta/ui/model/json/field/subtypes/PickListFieldMeta.java
+++ cxbox-starters/cxbox-starter-meta/src/main/java/org/cxbox/meta/ui/model/json/field/subtypes/PickListFieldMeta.java
@@ -24,7 +24,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@CxboxWidgetField("pickList")
+@CxboxWidgetField({"pickList", "suggestionPickList"})
 public class PickListFieldMeta extends FieldMetaBase {
 
 	private String popupBcName;
