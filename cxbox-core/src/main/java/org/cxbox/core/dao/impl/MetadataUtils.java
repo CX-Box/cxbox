5bcacac22f41faf7a53c07701acc6f70509c321f
IgorNB <igornb.mail@gmail.com>
2025-07-07 02:11:43 +0300

    CXBOX-956 | Remove deprecated tests and unused compile-testing dependency; migrate to `jakarta.annotation`. (#94)
    Co-authored-by: cxbox <cxbox.mail@gmail.com>

--- cxbox-core/src/main/java/org/cxbox/core/dao/impl/MetadataUtils.java
+++ cxbox-core/src/main/java/org/cxbox/core/dao/impl/MetadataUtils.java
@@ -46,7 +46,7 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-import javax.annotation.Nullable;
+import jakarta.annotation.Nullable;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
