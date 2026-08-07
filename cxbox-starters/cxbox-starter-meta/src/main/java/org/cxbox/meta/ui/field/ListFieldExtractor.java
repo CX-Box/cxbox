fe1d2ccf92c7ef2323fd0ef67aee8be7fd4f96cd
agelfman <agelfman@cxlab.ru>
2026-08-07 11:34:30 +0300

    CXBOX-1341 | Added. Added new wiget type Tree

--- cxbox-starters/cxbox-starter-meta/src/main/java/org/cxbox/meta/ui/field/ListFieldExtractor.java
+++ cxbox-starters/cxbox-starter-meta/src/main/java/org/cxbox/meta/ui/field/ListFieldExtractor.java
@@ -42,6 +42,7 @@ public class ListFieldExtractor extends BaseFieldExtractor {
 		for (final FieldMeta field : JsonUtils.readValue(FieldMeta[].class, widget.getFields())) {
 			widgetFields.addAll(extract(widget, field));
 		}
+	  customizeFields(widget, widgetFields);
 		return widgetFields;
 	}
 
@@ -82,4 +83,7 @@ public class ListFieldExtractor extends BaseFieldExtractor {
 		return 1;
 	}
 
+	public void customizeFields(WidgetDTO widget, Set<BcField> fields) {
+		// Extension point for widget families. Default implementation does nothing.
+	}
 }
