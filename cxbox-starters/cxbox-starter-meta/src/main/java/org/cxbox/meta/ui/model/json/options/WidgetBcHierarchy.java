0dddc25808167ed74ba8f54df7787f44fb4fe0ce
agelfman <agelfman@cxlab.ru>
2026-08-25 14:13:35 +0300

    CXBOX-1382 | Added. Added defaultFilter in Filter Group

--- cxbox-starters/cxbox-starter-meta/src/main/java/org/cxbox/meta/ui/model/json/options/WidgetBcHierarchy.java
+++ cxbox-starters/cxbox-starter-meta/src/main/java/org/cxbox/meta/ui/model/json/options/WidgetBcHierarchy.java
@@ -1,5 +1,5 @@
 /*
- * © OOO "SI IKS LAB", 2022-2026
+ * © OOO "SI IKS LAB", 2022-2023
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
